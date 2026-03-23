cae4eed8e43aca0eee1d53a5fe5985f73e226e6d
dviravrani-lab <dviravrani@gmail.co>
2026-03-23 17:30:51 +0200

    commit 11

--- api/controllers/MeetingController.js
+++ api/controllers/MeetingController.js
@@ -1,5 +1,4 @@
 const Meeting = require("../models/Meeting");
-const User = require("../models/User");
 
 module.exports = {
 
@@ -129,25 +128,6 @@ module.exports = {
         } catch (err) {
             res.status(500).json({ message: err.message });
         }
-    },
-
-     getUserByUid: async (req, res) => {
-        try {
-            const { uid } = req.params;
-
-            const user = await User.findOne({ firebaseUid: uid });
-
-            if (!user) {
-                return res.status(404).json({ message: "User not found" });
-            }
-
-            res.status(200).json(user);
-
-        } catch (err) {
-            res.status(500).json({ message: err.message });
-        }
     }
-
-
 
 };
--- api/routes/User.js
+++ api/routes/User.js
@@ -34,4 +34,24 @@ router.get("/", async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+
+router.get("/:uid", async (req, res) => {
+        try {
+            const { uid } = req.params;
+
+            const user = await User.findOne({ firebaseUid: uid });
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            res.status(200).json(user);
+
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    });
+
+
 module.exports = router;
